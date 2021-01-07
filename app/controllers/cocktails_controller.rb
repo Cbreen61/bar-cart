89a8bdb27375d198c7dcf1d18e96a6b03c461e99
Christopher Breen <C.jbreen61@gmail.com>
2021-01-07 15:15:27 -0500

    new cocktail form and default image

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,23 +3,38 @@ class CocktailsController < ApplicationController
     #create /post
     #New
     #make a get request to '/cocktails/new'
+    get '/cocktails/new' do 
+        erb :'/cocktails/new'
+    
+    end
 
     #Create
     #make a post request to '/cocktails'
+    post '/cocktails' do
+        cocktail = Cocktail.new(params)
+        if !cocktail.title.empty? && !cocktail.method.empty?
+            cocktail.save
+            redirect '/cocktails'
+        
+        else
+            @error = "Missing data. Please try again"
+            erb :'/cocktails/new'
+        end
+    end 
 
 
     #read /get 
     #index -all
     #make a get request to '/cocktails'
     get '/cocktails' do 
-        @cocktails = Cocktail.all
+        @cocktails = Cocktail.all.reverse
         erb :'cocktails/index'
     end
 
     #show- specific cocktail
     #make a get request to '/cocktails/:id'
     get '/cocktails/:id' do
-        @cocktail = Cocktail.find(the recipe id)
+        @cocktail = Cocktail.find(params["id"])
         erb :'/cocktails/show'
     end
 
---
+++ db/migrate/20210107201442_add_default_to_cocktail_image.rb
@@ -0,0 +1,4 @@
+class AddDefaultToCocktailImage < ActiveRecord::Migration
+  def change
+  end
+end
