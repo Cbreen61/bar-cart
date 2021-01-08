e8ec1c797d2670ebf6a97aaede252266cfc484e7
Christopher Breen <C.jbreen61@gmail.com>
2021-01-07 20:50:32 -0500

    added user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+   
+    #users can sign up
+    get '/signup' do
+        erb :'users/signup.erb'
+    end
+
+    post '/signup' do
+         
+    
+    
+    end
+
+
+
+    #users can delet account 
+
+end
