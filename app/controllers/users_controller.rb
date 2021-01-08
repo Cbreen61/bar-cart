7dddae69d5ee8a27c5943661bdf3ad93aefbf9b7
Christopher Breen <C.jbreen61@gmail.com>
2021-01-07 22:20:54 -0500

    sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,23 @@ class UsersController < ApplicationController
    
     #users can sign up
     get '/signup' do
-        erb :'users/signup.erb'
+        erb :'users/signup'
     end
 
     post '/signup' do
-         
+        user = User.new(params)
+        if user.username.empty? || user.password.empty?
+            @error = "Username and Password are invalid"
+            erb :'users/signup' 
+            
+        elsif User.find_by(username: user.username)
+            @error = "Account with that username is already in use"
+            erb :'users/signup'
+        else
+            user.save
+            session[:user_id] = user.id
+            redirect '/cocktails'
+        end
     
     
     end
